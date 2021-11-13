a56ec436111bd02beb45a75373aff1c9b9f2b33e
Eric Garcia <egarcia1868@gmail.com>
2021-11-12 22:09:55 -0600

    completed DOM lesson

--- Document_Object_Model/Part4_Game.js
+++ Document_Object_Model/Part4_Game.js
@@ -1,35 +1,24 @@
-// Restart Game Button
-var restart = document.querySelector('#b');
+// restart game button
+let restart = document.querySelector("#b");
 
-// Grab all the squares
-var squares = document.querySelectorAll("td");
-
-
-// Clear Squares Function
-function clearBoard() {
-  for (var i = 0; i < squares.length; i++) {
-      squares[i].textContent = '';
+let squares = document.querySelectorAll("td");
+// clears all the squares
+restart.addEventListener("click", function() {
+  for (i = 0; i < squares.length; i++) {
+    squares[i].textContent = ""
   }
+});
 
+function checker() {
+  if (this.textContent === "") {
+    this.textContent = "X"
+  } else if (this.textContent === "X") {
+    this.textContent = "O"
+  } else if (this.textContent === "O") {
+    this.textContent = ""
+  }
 }
-restart.addEventListener('click',clearBoard)
 
-
-
-
-// Create a function that will check the square marker
-function changeMarker(){
-    if(this.textContent === ''){
-      this.textContent = 'X';
-      // console.log(marker)
-    }else if (this.textContent === 'X') {
-      this.textContent = 'O';
-    }else {
-      this.textContent = '';
-    }
-};
-
-// Use a for loop to add Event listeners to all the squares
-for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', changeMarker);
+for(i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", checker)
 }
